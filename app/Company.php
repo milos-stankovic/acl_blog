8cb362d7e62ca259a81fbc45c2ac29606124182d
nikola <vidovic94@hotmail.com>
2018-01-09 10:48:44 +0100

    Register bug with name fixed.

--- app/Company.php
+++ app/Company.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'user_id'];
     public function projects()
     {
         return $this->hasMany(Project::class);
